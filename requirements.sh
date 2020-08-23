9af6dbaf6e38e31373f2d501913276eb20cf5e6a
Sándor Fodor <alex@digitaltrip.hu>
2020-08-23 11:55:03 +0100

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -2,7 +2,7 @@
 set -e
 
 ############################# INSTALL BEGINS ################################
-sudo pacman -S alacritty arc-gtk-theme arc-icon-theme dmenu bspwm rofi dunst catfish lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lua lxappereance mousepad nano neofetch nitrogen numlockx noto-fonts noto-fonts-emoji gvfs gvfs-afc gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gzip htop neofetch ntfs-3g pacman-contrib pcmanfm picom sxhkd ttf-dejavu xdg-user-dirs xorg-drivers zip
+sudo pacman -S alacritty arc-gtk-theme arc-icon-theme dmenu bspwm rofi dunst catfish lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lua lxappearance mousepad nano neofetch nitrogen numlockx noto-fonts noto-fonts-emoji gvfs gvfs-afc gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gzip htop neofetch ntfs-3g pacman-contrib pcmanfm picom sxhkd ttf-dejavu xdg-user-dirs xorg-drivers zip
 
 ############### FROM AUR ###############
 yay -S nerd-fonts-iosevka polybar mint-y-icons
