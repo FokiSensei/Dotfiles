7056466126b4db10bf7d164a809fec899a16778f
Sándor Fodor <alex@digitaltrip.hu>
2020-08-23 11:40:07 +0100

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -2,10 +2,10 @@
 set -e
 
 ############################# INSTALL BEGINS ################################
-sudo pacman -S alacritty arc-gtk-theme arc-icon-theme dmenu bspwm rofi dunst catfish lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lua mint-y-icons mousepad nano neofetch nitrogen numlockx noto-fonts noto-fonts-emoji gvfs gvfs-afc gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gzip htop neofetch ntfs-3g pacman-contrib pcmanfm picom sxhkd ttf-dejavu xdg-user-dirs xorg-drivers zip
+sudo pacman -S alacritty arc-gtk-theme arc-icon-theme dmenu bspwm rofi dunst catfish lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lua mousepad nano neofetch nitrogen numlockx noto-fonts noto-fonts-emoji gvfs gvfs-afc gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb gzip htop neofetch ntfs-3g pacman-contrib pcmanfm picom sxhkd ttf-dejavu xdg-user-dirs xorg-drivers zip
 
 ############### FROM AUR ###############
-yay -S nerd-fonts-iosevka polybar
+yay -S nerd-fonts-iosevka polybar mint-y-icons
 
 ################ Set graphical.target to default, and enable lightdm on start ####################
 sudo systemctl enable lightdm
