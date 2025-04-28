b61d215004758a7f159ac877beef98f7e4458800
FokiSensei <92307981+FokiSensei@users.noreply.github.com>
2025-04-29 00:22:18 +0200

    Update and rename nasmount_and_flatpak.sh to afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-# NAS driver mount és Flatpak programok telepítése
+# NAS driver mount
 sudo mkdir -p /mnt/media
 sudo mkdir -p /mnt/egyeb
 echo "//15.0.0.80/Media /mnt/media cifs username=Foki,password=a3K12hy12,uid=1000,gid=1000 0 0" | sudo tee -a /etc/fstab
 echo "//15.0.0.80/Other /mnt/egyeb cifs username=Foki,password=a3K12hy12,uid=1000,gid=1000 0 0" | sudo tee -a /etc/fstab
 sudo mount -a
 
+# Plex Media Server install and restorem
+paru -S plex-media-server
+sudo bash
+cp -r /home/foki/backup/Pelx* /var/lib/plex
+chown -R plex:plex /var/lib/plex
+find /var/lib/plex -type d -exec chmod 755 {} \;
+find /var/lib/plex -type f -exec chmod 644 {} \;
+systemctl start plexmediaserver
+systemctl enable plexmediaserver
+
+# install Flatpak apps
 flatpak install flathub com.github.tchx84.Flatseal
 flatpak install flathub com.heroicgameslauncher.hgl --user -y  flatpak install runtime/org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/23.08 --user -y
 flatpak override --user --filesystem=xdg-config/MangoHud:ro
